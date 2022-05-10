a737d4f606ea3a15f26e6f8d868a7c78ec4e2fba
Vadulia <honda2907@gmail.com>
2022-05-10 18:26:50 +0300

    add modal

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import { AppBar, Container, Toolbar, IconButton, Typography, Button, Grid, } from '@mui/material';
+import { AppBar, Container, Toolbar, IconButton, Typography, TextField, Button, Grid, Dialog, DialogTitle, DialogContent, DialogContentText, DialogActions, } from '@mui/material';
 import TourCards from "./TourCards";
 import Box from '@mui/material/Box';
 import MenuIcon from '@mui/icons-material/Menu';
 import BasicModal from './Modaal';
+import { TextFieldsRounded } from '@mui/icons-material';
+import React from 'react';
 
 
 
@@ -11,6 +13,15 @@ import BasicModal from './Modaal';
 
 
 function App() {
+
+  const [open, setOpen] = React.useState(false);
+
+  const handelClickOpen =() => {
+    setOpen (true);
+  }
+  const handelClose =() => {
+    setOpen (false);
+  }
 
   return (
     <>
@@ -23,10 +34,42 @@ function App() {
             <Typography variant='h6' sx={{ flexGrow: 1 }}> Test Project</Typography>
             <BasicModal />
             <Box mr={3}>
-              <Button color='inherit' variant='outlined'>Log In </Button>
+              <Button color='inherit' variant='outlined' onClick={handelClickOpen}>Log In </Button>
             </Box>
             <Button color='secondary' variant='contained'>Sign Up </Button>
+            
 
+            <Dialog open={open} onClose={handelClose} aria-labelledby="form-dialog-title">
+            <DialogTitle id= 'from-dialog-title'>Log In </DialogTitle>
+            <DialogContent>
+            <DialogContentText>Log in to see</DialogContentText>
+            <TextField
+              autoFocus
+              margin = 'dense'
+              id='name'
+              label='Email Adress'
+              type='email'
+              fullWidth
+              />
+
+             <TextField
+              autoFocus
+              margin = 'dense'
+              id='pass'
+              label='Password'
+              type='password'
+              fullWidth
+              />
+
+            </DialogContent>
+            <DialogActions>
+              <button onClick={handelClose} color='primary'> Cancel</button>
+              <button onClick={handelClose} color='primary'> Log In</button>
+            </DialogActions>
+
+
+
+            </Dialog>
           </Toolbar>
         </Container>
       </AppBar>
